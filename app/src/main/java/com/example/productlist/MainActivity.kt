c68bea52406367d21e2893da6862fdba913ead44
Jonathan Melo <jonathanmelo04@gmail.com>
2021-10-26 23:54:37 -0300

    adapter configuration in MainActivity class

--- app/src/main/java/com/example/productlist/MainActivity.kt
+++ app/src/main/java/com/example/productlist/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.productlist.adapter.AdapterProduto
+import com.example.productlist.model.Produto
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         val recyclerView_produtos = findViewById<RecyclerView>(R.id.recyclerView_produtos)
         recyclerView_produtos.layoutManager = LinearLayoutManager(this)
         recyclerView_produtos.setHasFixedSize(true)
+
+        //configuring the adapter
+        val listaProdutos: MutableList<Produto> = mutableListOf()
+        val adapterProduto = AdapterProduto(this,listaProdutos)
+        recyclerView_produtos.adapter = adapterProduto
     }
 }
